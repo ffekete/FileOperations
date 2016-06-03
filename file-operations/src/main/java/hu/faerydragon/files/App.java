56a435ab6b1d6ae9960c2e745a8975e4a714e927
Ferenc Fekete <Ferenc Fekete>
2016-06-03 13:10:36 +0200

    First version

---
+++ file-operations/src/main/java/hu/faerydragon/files/App.java
@@ -0,0 +1,13 @@
+package hu.faerydragon.files;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
